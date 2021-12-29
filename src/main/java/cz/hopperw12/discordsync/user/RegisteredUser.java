1d966b1ff5c16a50e10ede2bccce0deb129169a9
Kryštof Novosad <novosadkry@seznam.cz>
2021-12-29 17:25:48 +0100

    Complete UserManager and add lastOnline property to RegisteredUser

--- src/main/java/cz/hopperw12/discordsync/user/RegisteredUser.java
+++ src/main/java/cz/hopperw12/discordsync/user/RegisteredUser.java
@@ -1,10 +1,18 @@
 package cz.hopperw12.discordsync.user;
 
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
 import java.util.UUID;
 
 public class RegisteredUser {
     private UUID minecraftUUID;
     private long discordUUID;
+    private long lastOnline;
+
+    public RegisteredUser(Player player, long discordUUID) {
+        this(player.getUniqueId(), discordUUID);
+    }
 
     public RegisteredUser(UUID minecraftUUID, long discordUUID) {
         this.minecraftUUID = minecraftUUID;
@@ -17,5 +25,27 @@ public class RegisteredUser {
 
     public long getDiscordUUID() {
         return discordUUID;
+    }
+
+    public long getLastOnline() {
+        return lastOnline;
+    }
+
+    public void setLastOnline(long lastOnline) {
+        this.lastOnline = lastOnline;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RegisteredUser user = (RegisteredUser) o;
+        return Objects.equals(minecraftUUID, user.minecraftUUID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(minecraftUUID);
     }
 }
--- src/main/java/cz/hopperw12/discordsync/user/UserManager.java
+++ src/main/java/cz/hopperw12/discordsync/user/UserManager.java
@@ -2,25 +2,84 @@ package cz.hopperw12.discordsync.user;
 
 import cz.hopperw12.discordsync.DiscordSync;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserManager {
-
     private YamlConfiguration cfg;
+    private DiscordSync main;
 
     public UserManager() {
-        File file = new File(DiscordSync.getInstance().getDataFolder(), "users.yml");
+        main = DiscordSync.getInstance();
+        File file = new File(main.getDataFolder(), "users.yml");
 
         try {
-            if (!file.exists()) {
-                file.getParentFile().mkdirs();
-                file.createNewFile();
-            }
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+            cfg = YamlConfiguration.loadConfiguration(file);
+
         } catch (Exception e) {
+            main.getLogger().severe("Failed to load users.yml");
             e.printStackTrace();
         }
+    }
 
-        cfg = YamlConfiguration.loadConfiguration(file);
+    @SuppressWarnings("unchecked")
+    public void registerUser(RegisteredUser user) {
+        List<RegisteredUser> users = (List<RegisteredUser>) cfg.get("players");
+
+        if (users == null)
+            users = new ArrayList<>();
+
+        users.add(user);
+        cfg.set("players", users);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void unregisterUser(RegisteredUser user) {
+        List<RegisteredUser> users = (List<RegisteredUser>) cfg.get("players");
+
+        if (users == null)
+            users = new ArrayList<>();
+
+        users.remove(user);
+        cfg.set("players", users);
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean isRegistered(Player player) {
+        List<RegisteredUser> users = (List<RegisteredUser>) cfg.get("players");
+
+        if (users == null)
+            return false;
+
+        return users.stream()
+                .anyMatch(u -> u.getMinecraftUUID().equals(player.getUniqueId()));
+    }
+
+    @SuppressWarnings("unchecked")
+    public RegisteredUser get(Player player) {
+        List<RegisteredUser> users = (List<RegisteredUser>) cfg.get("players");
+
+        if (users == null)
+            return null;
+
+        return users.stream()
+                .filter(u -> u.getMinecraftUUID().equals(player.getUniqueId()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<RegisteredUser> getAll() {
+        List<RegisteredUser> users = (List<RegisteredUser>) cfg.get("players");
+
+        if (users == null)
+            users = new ArrayList<>();
+
+        return users;
     }
 }
