515dc7412a73a5c1d4c6b217f379b51ce007f305
Kryštof Novosad <novosadkry@seznam.cz>
2021-12-30 15:07:03 +0100

    better command link

--- src/main/java/cz/hopperw12/discordsync/commads/CommandLink.java
+++ src/main/java/cz/hopperw12/discordsync/commads/CommandLink.java
@@ -4,6 +4,8 @@ import cz.hopperw12.discordsync.DiscordSync;
 import cz.hopperw12.discordsync.requests.RequestManager;
 import cz.hopperw12.discordsync.requests.Token;
 import cz.hopperw12.discordsync.user.UserManager;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,18 +25,28 @@ public class CommandLink implements CommandExecutor {
 
         Player player = ((Player) sender);
 
-        if (userManager.isRegistered(player))
+        if (userManager.isRegistered(player)) {
             player.sendMessage("Už jsi zaregistrovaný!");
-
-        else if (requestManager.hasRequest(player)) {
-            Token token = requestManager.getRequest(player);
-            player.sendMessage("Použij tento token: " + token);
+            return false;
         }
 
-        else {
-            Token token = requestManager.addRequest(player);
-            player.sendMessage("Použij tento token: " + token);
-        }
+        if (!requestManager.hasRequest(player))
+            requestManager.addRequest(player);
+
+        Token token = requestManager.getRequest(player);
+
+        TextComponent msg = new TextComponent("Použíj tento token: ");
+        msg.setColor(ChatColor.DARK_GRAY);
+
+        TextComponent tokenMsg = new TextComponent(token.toString());
+        tokenMsg.setBold(true);
+        tokenMsg.setColor(ChatColor.GREEN);
+        tokenMsg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Klikni pro zkopírování do schránky").create()));
+        tokenMsg.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, token.toString()));
+
+        msg.addExtra(tokenMsg);
+
+        player.spigot().sendMessage(msg);
 
         return false;
     }
