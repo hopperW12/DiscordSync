804f0c092d3d3fa0ddcf2a273174dfc69e947e93
Kryštof Novosad <novosadkry@seznam.cz>
2021-12-30 18:16:05 +0100

    Add UserUnregisterListener listener

---
+++ src/main/java/cz/hopperw12/discordsync/listeners/UserUnregisterListener.java
@@ -0,0 +1,55 @@
+package cz.hopperw12.discordsync.listeners;
+
+import cz.hopperw12.discordsync.DiscordSync;
+import cz.hopperw12.discordsync.discord.Bot;
+import cz.hopperw12.discordsync.events.UserUnregisterEvent;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class UserUnregisterListener implements Listener {
+
+    @EventHandler
+    public void onUnregister(UserUnregisterEvent event) {
+        DiscordSync main = DiscordSync.getInstance();
+        Bot bot = main.bot;
+
+        Bukkit.dispatchCommand(
+            Bukkit.getConsoleSender(),
+            String.format("easywl remove %s", event.getUser().getPlayerName())
+        );
+
+        long guildID = main.getConfig().getLong("discord.guildID");
+        long roleID = main.getConfig().getLong("discord.roleID");
+
+        Guild guild = bot.jda.getGuildById(guildID);
+
+        if (guild == null) {
+            main.getLogger().warning("Defined guildID doesn't match any guilds");
+            return;
+        }
+
+        Member member = guild.getMemberById(event.getUser().getDiscordUUID());
+
+        if (member == null) {
+            return;
+        }
+
+        Role role = guild.getRoleById(roleID);
+
+        if (role == null) {
+            main.getLogger().warning("Defined roleID doesn't match any roles");
+            return;
+        }
+
+        guild.removeRoleFromMember(member, role).queue();
+        member.modifyNickname(null).queue();
+
+        member.getUser().openPrivateChannel().queue(channel ->
+            channel.sendMessage("Byl jsi odebrán z našeho serveru z důvodu neaktivity").queue()
+        );
+    }
+}
